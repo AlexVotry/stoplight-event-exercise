3862a9f92ef8a127ac24508c13dd33975a342291
Alex Votry <alexvotry@gmail.com>
2016-02-24 07:24:03 -0800

    cleaned up the variables

--- alt.js
+++ alt.js
@@ -1,17 +1,12 @@
 var button = document.getElementById('controls');
-var stop = document.getElementById('stopLight');
-var slow = document.getElementById('slowLight');
-var go = document.getElementById("goLight");
+var cmd = document.getElementsByClassName('bulb');
 var btnId= ["stopButton", "slowButton", "goButton"];
-var cmd = [stop, slow, go];
 var colors = ["red", "yellow", "green"];
 
-console.log(cmd);
 function checkButton(event) {
-  var btn = event.target.id; 
+  var btn = event.target.id;
   for (var i = 0; i < btnId.length; i++) {
     if (btn === btnId[i]) {
-      console.log(cmd[i]);
       if (cmd[i].style.backgroundColor === colors[i]) {
         cmd[i].style.backgroundColor = "#111";
         } else {
